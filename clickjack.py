c133b1705828f0c9980b3fc6fb57749e953756c6
saleem8054 <transparent79twister@gmail.com>
2021-02-04 22:14:16 +0100

    Added colors

--- clickjack.py
+++ clickjack.py
@@ -2,8 +2,6 @@ from urllib.request import urlopen
 from sys import argv, exit
 import threading
 from colorama import Fore
-f=open("Vulnerable.txt", "a+")
-
 
 def check(url):
     ''' check given URL is vulnerable or not '''
@@ -11,7 +9,7 @@ def check(url):
     try:
         if "http" not in url: url = "http://" + url
 
-        data = urlopen(url)
+        data = urlopen(url,timeout=3)
         headers = data.info()
 
         if not "X-Frame-Options" in headers: return True
@@ -21,22 +19,22 @@ def check(url):
 
 
 def listVulnerableSite(url):
+    f=open("Vulnerable.txt", "a+")
     f.write(url+"\n")
-
+    f.close()
 
 def main():
     try: sites = open(argv[1], 'r').readlines()
-    except: print("[*] Usage: python(3) clickjacking_tester.py <file_name>"); exit(0)
+    except: print("[*] Usage: python3 clickjacking_tester.py <file_name>"); exit(0)
 
     for site in sites[0:]:
         status = check(site)
 
         if status:
-            print("[+] "+Fore.GREEN+site.split('\n')[0] +Fore.WHITE+ " is "+Fore.RED+"vulnerable"+Fore.WHITE+" to ClickJacking")
+            print(Fore.RED+"[+] "+Fore.GREEN+site.split('\n')[0] +Fore.WHITE+ " is "+Fore.RED+"Vulnerable")
             listVulnerableSite(site.split('\n')[0])
         
-        elif not status: print("[+] "+Fore.GREEN+site.split('\n')[0] +Fore.WHITE+ " is "+Fore.CYAN+" not vulnerable"+Fore.WHITE+" to ClickJacking")
-        else: print('Every single thing is crashed, Python got mad, dude wtf you just did?')
-    f.close()
+        elif not status: print(Fore.CYAN+"[-] "+Fore.GREEN+site.split('\n')[0] +Fore.WHITE+ " is "+Fore.CYAN+" NOT Vulnerable")
+        else: print(Fore.CYAN+'Every single thing is crashed, Python got mad, dude wtf you just did?')
+ 
 if __name__ == '__main__': main()
-
